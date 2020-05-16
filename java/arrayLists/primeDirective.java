1dd13ddcb6cac76876c0599162107fa92acc0ec2
pixelynx <tjstevens1992@gmail.com>
2020-05-15 22:54:19 -0400

    change

--- java/arrayLists/primeDirective.java
+++ java/arrayLists/primeDirective.java
@@ -23,7 +23,7 @@ class PrimeDirective {
 
     public static void main(String[] args) {
 
-        // PrimeDirective pd = new PrimeDirective();
+        PrimeDirective pd = new PrimeDirective();
         int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
 
         System.out.println(onlyPrimes(numbers));
